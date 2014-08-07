d5f75c4b75872afef1effe2630c7df538d68b812
kahlen <kahlen.lin@gmail.com>
2014-08-07 17:29:58 +0100

    MQTT performance test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,9 @@ import play.mvc.Http
 object Application extends Controller {
 
   def index = Action { request =>
+    // for test
+//    initClient
+
     request.cookies.get("userId") match {
       case None =>
         Ok(views.html.index("Hello, world"))
@@ -32,10 +35,9 @@ object Application extends Controller {
   import org.eclipse.paho.client.mqttv3.MqttTopic
   var clientMQ: MqttClient = _
   var receiveCount = 0
+  var sendCount = 0
 
-  def testMqttPerformance(qos: Integer) = Action {
-    val topic = "mqtt-malaria/#"
-    Logger.debug("start to test, subscribe topic(" + topic + ") with Qos " + qos)
+  def initClient() = {
     try {
       // Eclipse Paho + HiveMQ
       val brokerUrl: String = "tcp://localhost:1883"
@@ -53,7 +55,6 @@ object Application extends Controller {
 
       //Connect to MqttBroker
       clientMQ.connect( mqttOptions )
-      clientMQ.subscribe(topic, qos)
 
       //Callback automatically triggers as and when new message arrives on specified topic
       var callback: MqttCallback = new MqttCallback() {
@@ -66,7 +67,7 @@ object Application extends Controller {
         }
 
         override def deliveryComplete(arg0: IMqttDeliveryToken) {
-          System.err.println("delivery complete")
+//          System.err.println("delivery complete")
         }
 
         override def connectionLost(arg0: Throwable) {
@@ -81,13 +82,74 @@ object Application extends Controller {
     } catch {
       case e: Exception => e.printStackTrace()
     }
+  }
+
+  val requestTotal = 100
+  var sendTime = new Array[Long](requestTotal)
+  var receiveTime = new Array[Long](requestTotal)
+  def testMqttPerformanceAuto(qos: Integer) = Action {
+    try {
+      val topic = "user/" + sendCount
+      // 100 bytes
+      val message = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+      val mTopic: MqttTopic = clientMQ.getTopic(topic);
+      val mMessage: MqttMessage = new MqttMessage(message.getBytes());
+      mMessage.setQos(qos);
+      mTopic.publish(mMessage);
+      val currentTime = java.lang.System.currentTimeMillis
+      sendTime(sendCount) = currentTime
+      System.err.println( "%4d".format(sendCount) + ": " + currentTime)
+      sendCount += 1
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+
+    Ok("Hello, world!")
+  }
+
+  def testMqttPerformancePong(count: Integer) = Action {
+    val currentTime = java.lang.System.currentTimeMillis
+    receiveTime(count) = currentTime
+    System.err.println( "%4d".format(count) + ": " + currentTime)
+    Ok("Hello, world!")
+  }
+
+  def getLatency() = Action {
+    var latencySum = 0l
+    var lostCount = 0
+    for ( i <- 0 until requestTotal) {
+      if ( receiveTime(i) != 0l ) {
+        latencySum = latencySum + ( receiveTime(i) - sendTime(i) )
+      } else {
+        lostCount += 1
+      }
+    }
+
+    val latencyAve = latencySum.toDouble/(requestTotal-lostCount)
+    val output = new StringBuilder("latency sum: " + latencySum + "\n")
+    output ++= "latency average: " + latencyAve + "\n"
+    output ++= "lost: " + lostCount + "\n"
+    Ok(output.toString)
+  }
+
+  def testMqttPerformance(qos: Integer) = Action {
+    val topic = "mqtt-malaria/#"
+    Logger.debug("start to test, subscribe topic(" + topic + ") with Qos " + qos)
+    try {
+      clientMQ.subscribe(topic, qos)
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
 
     Ok("Hello, world!")
   }
 
   def endMqttPerformanceTest() = Action {
     Logger.debug("end test")
+    sendTime = new Array[Long](requestTotal)
+    receiveTime = new Array[Long](requestTotal)
     receiveCount = 0
+    sendCount = 0
     if ( clientMQ != null && clientMQ.isConnected )
       clientMQ.disconnect
     Ok("Bye, world!")
